8b239b2e935b6d38b4fb2cb350cfcda6e70c142e
seveibar <seveibar@gmail.com>
2025-01-20 10:52:22 -0800

    bootstrapping function

---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+import type { CircuitJson } from "circuit-json"
+
+export const generateAutoroutingCacheKey = (
+  circuitJson: CircuitJson,
+  options: { subcircuit_id: string },
+) => {
+  // TODO
+}
