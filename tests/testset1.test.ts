ae11c9455de4ad8a799c7f65496462009330ef66
seveibar <seveibar@gmail.com>
2025-01-21 15:46:00 -0800

    add snapshot to testset1

--- tests/testset1.test.ts
+++ tests/testset1.test.ts
@@ -10,5 +10,81 @@ test("testset1", () => {
   const normalizedAutoroutingJson2 =
     convertCircuitJsonToNormalizedAutoroutingJson(circuit2 as any)
 
+  expect(normalizedAutoroutingJson1).toMatchInlineSnapshot(`
+{
+  "allowed_layers": 1,
+  "nets_to_route": [
+    3,
+  ],
+  "sorted_normalized_objects": [
+    {
+      "height": "0.60",
+      "layers": [
+        "top",
+      ],
+      "net": 2,
+      "type": "pad",
+      "width": "0.60",
+      "x": "-2.00",
+      "y": "0.00",
+    },
+    {
+      "height": "0.60",
+      "layers": [
+        "top",
+      ],
+      "net": 3,
+      "type": "pad",
+      "width": "0.60",
+      "x": "-3.00",
+      "y": "0.00",
+    },
+    {
+      "height": "0.60",
+      "layers": [
+        "top",
+      ],
+      "net": 3,
+      "type": "pad",
+      "width": "0.60",
+      "x": "3.00",
+      "y": "0.00",
+    },
+    {
+      "height": "0.60",
+      "layers": [
+        "top",
+      ],
+      "net": 1,
+      "type": "pad",
+      "width": "0.60",
+      "x": "4.00",
+      "y": "0.00",
+    },
+  ],
+  "sorted_normalized_traces": [
+    {
+      "net": 3,
+      "route": [
+        {
+          "layers": [
+            "top",
+          ],
+          "x": "3.00",
+          "y": "0.00",
+        },
+        {
+          "layers": [
+            "top",
+          ],
+          "x": "-3.00",
+          "y": "0.00",
+        },
+      ],
+    },
+  ],
+}
+`)
+
   expect(normalizedAutoroutingJson1).toEqual(normalizedAutoroutingJson2)
 })
